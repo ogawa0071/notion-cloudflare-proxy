6654a2447fe9be8b452f77bda7b3d6759b8f97fa
Futa Ogawa <ogawa0071@gmail.com>
2024-06-26 04:52:33 +0900

    Update for Notion Sites

--- src/worker.ts
+++ src/worker.ts
@@ -1,42 +1,58 @@
 export interface Env {
-  HOME_ID: string;
+  NOTION_DOMAIN: string;
 }
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
-    const _url = new URL(`https://www.notion.so${url.pathname}${url.search}`);
+    const _url = new URL(url);
+    _url.hostname = env.NOTION_DOMAIN;
 
-    if (url.pathname === '/') {
-      return Response.redirect(new URL(`/${env.HOME_ID}`, url).toString(), 308);
+    if (request.method === 'POST' && url.pathname === '/api/v3/getPublicPageData') {
+      const requestBody = await request.json<Record<string, any>>();
+      requestBody.requestedOnExternalDomain = false;
+      requestBody.spaceDomain = env.NOTION_DOMAIN.replace('.notion.site', '');
+
+      const _request = new Request(request, {
+        body: JSON.stringify(requestBody),
+      });
+
+      const response = await fetch(_url, _request);
+      const responseBody = await response.json<Record<string, any>>();
+      responseBody.publicDomainName = url.hostname;
+
+      return new Response(JSON.stringify(responseBody), response);
+    }
+
+    if (request.method === 'POST' && url.pathname === '/api/v3/syncRecordValues') {
+      const response = await fetch(_url, request);
+      const responseBody = await response.json<{ recordMap: { site: Record<string, any>; public_domain: Record<string, any> } }>();
+
+      try {
+        responseBody.recordMap.site = Object.fromEntries(
+          Object.entries(responseBody.recordMap.site).map(([key, value]) => {
+            value.value.value.header.hideWatermark = true;
+            return [key, value];
+          })
+        );
+      } catch {}
+
+      try {
+        responseBody.recordMap.public_domain = Object.fromEntries(
+          Object.entries(responseBody.recordMap.public_domain).map(([key, value]) => {
+            if (value.value.value.domain_name === env.NOTION_DOMAIN.replace('.notion.site', '')) {
+              value.value.value.domain_name = url.hostname;
+              value.value.value.domain_type = 'custom';
+            }
+            return [key, value];
+          })
+        );
+      } catch {}
+
+      return new Response(JSON.stringify(responseBody), response);
     }
 
     const response = await fetch(_url, request);
-
-    if (_url.pathname === '/api/v3/getPublicPageData') {
-      const json = await response.json<{ [key: string]: any }>();
-      delete json.requireInterstitial;
-      return new Response(JSON.stringify(json), response);
-    }
-
-    const _body = new HTMLRewriter().on('body', new ElementHandler(url.hostname)).transform(response).body;
-    return new Response(_body, {
-      ...response,
-      status: response.status === 404 && response.headers.get('content-type')?.includes('text/html') ? 200 : response.status,
-    });
+    return response;
   },
 };
-
-class ElementHandler {
-  publicDomainName: string;
-
-  constructor(publicDomainName: string) {
-    this.publicDomainName = publicDomainName;
-  }
-
-  element(element: Element) {
-    element.append(`<script>window.CONFIG.publicDomainName = '${this.publicDomainName}';</script>`, {
-      html: true,
-    });
-  }
-}
